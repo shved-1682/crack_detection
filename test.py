22eb32df5ea51e041c28204ff98e8805381ee7f3
shved-1682 <vladislav.prm@gmail.com>
2023-04-16 10:59:55 +0500

    refactor: 💡 Rewrite test.py to find number of cracks
    Create new function find_crack

--- test.py
+++ test.py
@@ -2,35 +2,38 @@ import numpy as np
 import skimage
 from matplotlib import pyplot as plt
 
-image = skimage.io.imread("img_gabora/expls/1_50%_x1(1).jpg", as_gray=True)
-height = image.shape[0]
-width = image.shape[1]
 
-start = (height / 2, 0)  # Start of the profile line row=100, col=0
-end = (height / 2, width - 1)  # End of the profile line row=100, col=last
+def find_crack(path: str):
+    SAVE_PATH = "./img_gabora/expls"
+    crack_list = []
+    image = skimage.io.imread(path, as_gray=True)
+    height = image.shape[0]
+    width = image.shape[1]
 
-# profile intensity
-profile = skimage.measure.profile_line(image, start, end)
+    file_name = path.split("/")[-1]
 
-crack_count = np.sum(profile == 255) // 2
-print(crack_count)
+    for coef in np.arange(0.2, 0.8, 0.05):
+        start = (height * coef, 0)  # Start of the profile line row=100, col=0
+        end = (height * coef, width - 1)  # End of the profile line row=100, col=last
 
-# Image profile intensity
-fig1 = plt.plot(profile)
-plt.savefig('img/expls/test1.jpg')
+        # profile intensity
+        profile = skimage.measure.profile_line(image, start, end)
+        crack_count = np.sum(profile == 255) // 2
+        crack_list.append(crack_count)
 
-# Image :)
-_, ax = plt.subplots()
-ax.imshow(image)
-plt.savefig('img/expls/test2.jpg')
+        # Image profile intensity
+        fig, ax = plt.subplots(1, 2)
+        ax[0].set_title('Image')
+        ax[0].imshow(image)
+        ax[0].plot([start[1], end[1]], [start[0], end[0]], 'r')
+        ax[1].set_title('Profile')
+        ax[1].plot(profile)
+        plt.savefig(f"{SAVE_PATH}/{coef}_{file_name}")
+
+    print(crack_list)
+    print(sum(crack_list) / len(crack_list))
 
 
-# fig, ax = plt.subplots(1, 2)
-# ax[0].set_title('Image')
-# ax[0].imshow(image)
-# ax[0].plot([start[1], end[1]], [start[0], end[0]], 'r')
-# ax[1].set_title('Profile')
-# ax[1].plot(profile)
-# fig.savefig('img/expls/test.jpg')
-
-
+find_crack(path="./img_gabora/expls/1_0%_crack_1x (2).jpg")
+# img\expls\1_0%_crack_1x (2).jpg
+# img_gabora\expls\1_0%_crack_1x (2).jpg
